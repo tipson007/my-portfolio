62183271763bfc06263c76d02d33896452bd6398
“tipson007” <kooldec@yahoo.co.uk>
2025-08-14 10:02:53 +0100

    update code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
 import initTilt from './js/tilt';
 import initSr from './js/sr';
+import { profileImage, awsImage, projectImage, quizImage, uxImage, favicon } from './js/images';
 import './style/main.scss';
+
+// Set favicon
+document.querySelector('link[rel="shortcut icon"]').href = favicon;
+
+// Set up images after DOM is loaded
+document.addEventListener('DOMContentLoaded', () => {
+  // Profile image
+  document.querySelector('#about img').src = profileImage;
+  
+  // Project images
+  const projectImages = document.querySelectorAll('.project-wrapper__image img');
+  projectImages.forEach((img, index) => {
+    switch(index) {
+      case 0:
+        img.src = awsImage;
+        break;
+      case 1:
+        img.src = projectImage;
+        break;
+      case 2:
+        img.src = quizImage;
+        break;
+      case 3:
+        img.src = uxImage;
+        break;
+    }
+  });
+});
 
 $('a[href^="#"]').on('click', function(event) {
   var target = $(this.getAttribute('href'));
---
+++ src/js/images.js
@@ -0,0 +1,16 @@
+import profileImage from '../assets/profile.jpg';
+import awsImage from '../assets/aws.jpg';
+import projectImage from '../assets/project.jpg';
+import quizImage from '../assets/quiz.jpg';
+import uxImage from '../assets/ux.jpg';
+import favicon from '../assets/favicon.png';
+
+// Export all images
+export {
+  profileImage,
+  awsImage,
+  projectImage,
+  quizImage,
+  uxImage,
+  favicon
+};
